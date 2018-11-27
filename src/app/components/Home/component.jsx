1961dc47bbcee58c01658df2c376d3d870695778
Tyron Boyd <tyron.boyd@objectconsulting.com.au>
2018-11-27 11:20:23 +1100

    Commit app

--- src/app/components/Home/component.jsx
+++ src/app/components/Home/component.jsx
@@ -1,36 +1,85 @@
-import React from 'react';
-import { Link } from 'react-router';
+
+import * as React from 'react';
+import BetEasyConfig from '../../../assets/js/bet-easy.config.js';
+import styles from './styles.scss';
 
 export default class Home extends React.Component {
+
+	constructor() {
+		super();
+		this.state = { raceData: [] };
+	}
+	/**
+	 * Component did mount called when the app first loads.
+	 * An interval is set to update from the API (configurable in the config file).
+	 *
+	 * @returns void
+	 */
+	componentDidMount() {
+		this.initData();
+		setInterval(() => {
+			this.initData();
+		}, 5000);
+	}
+
+    /**
+     * Initialises marker data
+     *
+     * @returns {Promise} The marker data
+     */
+	initData() {
+		this.getRaceData()
+			.then(raceData => {
+				this.setState({
+					raceData: raceData.result
+				});
+			});
+	}
+
+	/**
+	 * fetch Bet Easy race data
+	 *
+	 * @returns {Promise} The Bet Easy data
+	 */
+	getRaceData() {
+		const betEasyUrl = BetEasyConfig.apiUrl;
+		return new Promise(resolve =>
+			fetch(betEasyUrl)
+				.then(response => response.json())
+				.then(data => {
+					resolve(data);
+				})
+		);
+	}
+
+	getTimeUntilRace(date) {
+		const calculateTimeUntilRace = new Date(date) - new Date();
+		const diffDays = Math.floor(calculateTimeUntilRace / 86400000); // days
+		const diffHrs = Math.floor((calculateTimeUntilRace % 86400000) / 3600000); // hours
+		const diffMins = Math.round(((calculateTimeUntilRace % 86400000) % 3600000) / 60000); // minutes
+		return `${diffDays}d ${diffHrs}h ${diffMins}m`;
+	}
+
 	render() {
+		const listItems = this.state.raceData.map((data) =>
+			<tr key={data.EventID}>
+				<td><img role="presentation" src="../../../assets/img/dog.png" /></td>
+				<td>{data.EventName}</td>
+				<td>{data.Venue.Venue}, {data.Venue.StateCode}<br />{data.Venue.Slug}</td>
+				<td>{this.getTimeUntilRace(data.AdvertisedStartTime)}</td>
+			</tr>
+		);
 		return (
-			<div className="container home">
-				Launchpad for maps
-				<div>
-					<ul>
-						<li>
-							<Link to="/probe-map">Probe map</Link>
-						</li>
-						<li>
-							<Link to="/los-map">Los map</Link>
-						</li>
-						<li>
-							<Link to="/message-count">Message Count Graphs</Link>
-						</li>
-						<li>
-							<Link to="/qkz/melbourne/0">QKZ Melbourne Diagram no options</Link>
-						</li>
-						<li>
-							<Link to="/qkz/melbourne/0,2">QKZ Melbourne Diagram with options</Link>
-						</li>
-						<li>
-							<Link to="/qkz/sydney/0">QKZ Sydney Diagram no options</Link>
-						</li>
-						<li>
-							<Link to="/qkz/sydney/0,2">QKZ Sydney Diagram with options</Link>
-						</li>
-					</ul>
+			<div className={styles.container}>
+				<img role="presentation" src="../../../assets/img/logo.png" />
+				<div className={styles['div-container']}>
+					<h3 className={styles.heading}>Next to Jump</h3>
 				</div>
+				<table className="table">
+					<tbody>
+						{listItems}
+					</tbody>
+				</table>
 			</div>
 		);
 	}
---
+++ src/assets/js/bet-easy.config.js
@@ -0,0 +1,6 @@
+/**
+ * Contains the config options for the Bet Easy application.
+ */
+export default {
+	apiUrl: 'https://s3-ap-southeast-2.amazonaws.com/bet-easy-code-challenge/next-to-jump'
+}
